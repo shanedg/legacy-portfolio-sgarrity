744df51c310f7f90d5304470aff1797234a87081
shane <shanedgarrity@gmail.com>
2019-03-05 23:55:14 -0500

    oh wow apparently i do have the source

--- dist/js/ui.js
+++ dist/js/ui.js
@@ -1,1 +1,32 @@
 console.log('js');
+
+window.addEventListener('load', function() {
+  let details = document.querySelectorAll('.work .skills');
+  details.forEach(function(element) {
+    element.style.maxHeight = '0px';
+  });
+
+  let toggles = document.querySelectorAll('.fa-plus');
+  toggles.forEach(function(element) {
+    element.addEventListener('click', function(t) {
+
+      console.log(t.target.parentElement.nextSibling.nextSibling);
+
+      if (t.target.parentElement.nextSibling.nextSibling.style.maxHeight === '0px') {
+        t.target.parentElement.nextSibling.nextSibling.style.maxHeight = '999px';
+        t.target.className = 'fa fa-minus';
+      } else {
+        t.target.parentElement.nextSibling.nextSibling.style.maxHeight = '0px';
+        t.target.className = 'fa fa-plus';
+      }
+
+    });
+
+  });
+
+  let first = document.querySelector('.fa-plus');
+  first.className = 'fa fa-minus';
+  first.parentElement.nextSibling.nextSibling.style.maxHeight = '999px';
+
+
+});
